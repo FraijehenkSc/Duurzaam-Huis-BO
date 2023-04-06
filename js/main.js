cf3c17f352a33e425f3e4b674f36fa3527bfec5f
Joost <35542@ma-web.nl>
2023-04-06 12:19:08 +0200

    temperatuur werkt bij 2e blokje

--- js/main.js
+++ js/main.js
@@ -4,10 +4,22 @@ const search = document.querySelector("#search")
 const weather = document.getElementById("js--weather")
 const getWeather = async(city) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    const url2 = `https://api.openweathermap.org/data/2.5/weather?q=$amsterdam&appid=${API_KEY}&units=metric`
+    console.log(url2);
     const response = await fetch(url);
     const data = await response.json();
     console.log(url);
     return showWeather(data)
+}
+
+
+const getWeatherA = async() => {
+    const url2 = `https://api.openweathermap.org/data/2.5/weather?q=amsterdam&appid=${API_KEY}&units=metric`
+    console.log(url2);
+    const responseA = await fetch(url2);
+    const dataA = await responseA.json();
+    console.log(url2);
+    return showWeatherA(dataA);
 }
 
 
@@ -28,10 +40,20 @@ form.addEventListener(
     "submit",
     function(event){
         getWeather(search.value);
+        getWeatherA(search.value);
         event.preventDefault();
     }
 
 )
 
+const weatherA = document.getElementById("js--temperatureA")
+const showWeatherA = (dataA) => {
+    console.log(dataA);
+    weatherA.innerText = dataA.main.temp + " ℃"
+}
 
+const test = function(){
+    getWeatherA(search.value);
+}   
 
+test();
